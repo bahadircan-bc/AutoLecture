283414d6d2dd8afad09be4e3a06f8d56d1f8826e
bahadircan-bc <81996626+bahadircan-bc@users.noreply.github.com>
2023-03-02 11:48:15 +0300

    methods written

--- main.py
+++ main.py
@@ -4,135 +4,212 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from datetime import datetime
-from time import sleep
+import time
 
-if __name__ == '__main__':
+# fTODO: Когда дни пустые
+# TODO: Копирование с сайта
 
-    # get current time
-    now = datetime.now()
-    # get current day name (eg. Monday, Tuesday, ...)
-    current_day = now.strftime("%A")
 
-    # read classes.txt for day names
-    with open('classes.txt') as f:
-        lines = f.readlines()
+def getTodaysLessonHours():
+    dateNow = datetime.now()
+    currentDay = dateNow.strftime("%A")
+
+    def dateSort(date):
+        return datetime.strptime(date, '%H:%M:%S')
+    
+    with open('classes.txt') as file:
+        lines = file.readlines()
         for line in lines:
-            line.strip()
-            if current_day in line:
-                # if current day name matches first token in a line then there is lecture today
-                start_hour_string = line.split(' ')[1].strip()
-                break
-        else:
-            # will execute if for loop never reaches break statement
-            # which means there is no lecture today
-            print('no lecture today')
-            input('Press Enter to exit...')
-            exit()
-    # if there is lecture today check for deltatime until the lecture start
-    start_time = datetime.strptime(f'{datetime.now().strftime("%d/%m/%y")} {start_hour_string}', '%d/%m/%y %H:%M:%S')
-    # if lecture has not started yet, sleep until it starts
-    if start_time > now:
-        print(f'sleeping until {start_hour_string}')
-        sleep((start_time-now).total_seconds())
+            line = line.strip()
 
-    # init driver
-    dr = webdriver.Firefox()
-    # implicit wait forces driver to wait if it can not find an element
-    dr.implicitly_wait(10)
-    # wait object is going to use for explicit wait commands
-    wait = WebDriverWait(dr, 10)
-    # get function makes a request to the url
-    dr.get('https://online.yildiz.edu.tr')
-    # current handle is going to be used later to change tabs
-    original_window = dr.current_window_handle
+            # Eğer gün ya da saat yoksa
+            if currentDay in line and currentDay != line:
+                try:
+                    lecture_hours = line.split(' ')[1:]
+                    lecture_hours.sort(key=dateSort)
+                    break
+                except:
+                    print('!!! WARNING !!!')
+                    print('Incorrect time format\n')
+                    print('Please write in that form:')
+                    print('  Day time1 time2 ... timeN\n')
+                    print('Example:')
+                    print('  Monday 09:00:00 10:00:00 15:30:00\n')
+                    exit()
+        else:
+            print('No lecture today')
+            exit()
+
+    return lecture_hours
+
+def getLoginAndPassword():
+    login = ''
+    password = ''
 
     try:
-        print('1')
-        # try to find textbox for mail query
-        textbox_mail = wait.until(
-            EC.presence_of_element_located((By.ID, 'Data_Mail'))
-        )
-        print('2')
-        # try to find textbox for password query
-        textbox_password = wait.until(
-            EC.presence_of_element_located((By.ID, 'Data_Password'))
-        )
-        print('3')
-        # try to find button for login
-        login_button = wait.until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/main/div/div/div/div/div/div/form/div[3]/div[2]/button'))
-        )
-        print('4')
-        # fill in the mail
-        textbox_mail.send_keys('bahadir.can@std.yildiz.edu.tr')
-        print('5')
-        # fill in the password
-        textbox_password.send_keys('Bn7r6yPa?7')
-        print('6')
-        # click login
-        login_button.click()
-        
-        # the welcome page includes a timeline element
-        # which includes timeline-row elements for each upcoming lessons
-        # however timeline-row elements are not clickable since timeline element overlaps them
-        lecture_button = dr.find_element(By.CLASS_NAME, 'timeline-row')
-        print('7')
-        # thus the driver clicks timeline-content elements
-        lecture_button.find_element(By.CLASS_NAME, 'timeline-content').click()
-        print('8')
-        # now it tries to find `derse katil` button
-        # here it is not possible to find the button using classname or tagname or id
-        # hence we use the xpath of the button since the view is not going to change in any other occurances
-        dr.find_element(By.XPATH, '/html/body/main/div/div[1]/div/div[7]/div/div/div/div/div/table/tbody/tr/td[5]/a').click()
-        print('9')
-        # once we click the button the link is going to be opened in a new tab
-        # so we need to change tabs
-        # first we need to wait until there are two windows
-        wait.until(EC.number_of_windows_to_be(2))
-        # we iterate over the window_handles and find the first one that does not match the initial window
-        for window_handle in dr.window_handles:
-        # here the original_window variable comes into play
-            if window_handle != original_window:
-                dr.switch_to.window(window_handle)
-                break
-        # it takes a while to load the second window so we wait until the title bar changes
-        # the title bar change can be acquired using a check to see if it includes 'Zoom'
-        wait.until(EC.title_contains('Zoom'))
-        # the alert that comes up to allow the driver to launch Zoom app can not be accepted using selenium
-        # because the alert is an OS level alert
-        # accepting the alert can be achieved using other packages (eg. pyAutoGui)
-        # but using such libraries makes the app not usable headless
-        # (being headless meaning the window needs to be seen on top of the screen at all times)
-        current_url = dr.current_url
-        # so we modify the current url to launch the zoom app on the browser
-        new_url = current_url.replace('w/', 'wc/join/')
-        dr.get(new_url)
-        print('10')
-        # when zoom launches on the browser we now need to fill in the username and then join the call
-        # in my case my name was already filled in automatically by the OS preferences
-        # if a change is required it can be achieved with following steps
-        # find the text box using driver.find_element()
-        # type your name using element.send_keys()
-        # then resume the program as is
-        join_button = wait.until(EC.element_to_be_clickable((By.ID, 'joinBtn')))
-        join_button.click()
-        # These were forgotten to be removed during debugging you can ignore following 3 lines
-        # alert = wait.until(
-        #     EC.alert_is_present()
-        # )
-    except Exception as e:
-        # if any error occures while the program is running
-        # it is going to be caught in these lines
-        # printed on the screen then exit the program
-        print(e)
-        input('Press Enter to exit...')
-        dr.quit()
-        exit()
-    else:
-        pass
+        with open('classes.txt') as file:
+            lines = file.readlines()
 
-    # if the lecture fires up with no problem the program is going to be halted
-    input('Press Enter to exit...')
-    dr.quit()
+            for line in lines:
+                line = line.strip()
+
+                if 'Login:' in line and "Login:" != line:
+                    try:
+                        login = line.split(' ')[1]
+                        break
+                    except:
+                        print('Something went wrong. Please control your date file and login.')
+            else:
+                print('Please enter login')
+                exit()
+
+            for line in lines:
+                line = line.strip()
+
+                if 'Password:' in line and "Password:" != line:
+                    try:
+                        password = line.split(' ')[1]
+                        break
+                    except:
+                        print('Something went wrong. Please control your date file and password.')
+            else:
+                print('Please enter password')
+                exit()
+    except:
+        print('ERROR: Control your path and cwd (working directory)')
+        exit()
+
+    return (login, password)
+
+
+
+def getClosestHour(hours):
+    #* Şu anki saate en yakın saati buluyor ve bu saat ve sonrasını döndürüyor
+
+    # test için
+    # start_hour_string = '10:00:00'
+    # now = datetime.strptime(f'{datetime.now().strftime("%d/%m/%y")} {start_hour_string}', '%d/%m/%y %H:%M:%S')
+    # dateNowInMin = now.hour * 60 + now.minute
+    
+    dateNowInMin = datetime.now().hour * 60 + datetime.now().minute
+    diff = 99999
+    index = -1
+
+    for hour in hours:
+        start_time = datetime.strptime(hour, '%H:%M:%S')
+        lessonHourInMin = start_time.hour * 60 + start_time.minute
+        # print(f'Lesson hour {hour} in minutes - {lessonHourInMin}')
+        
+        if diff > abs(lessonHourInMin - dateNowInMin):
+            diff = abs(lessonHourInMin - dateNowInMin)
+            index = hours.index(hour)
+        
+    return hours[index:]
+
+
+
+def waitUntil(start):
+    now = datetime.now()
+
+    if start > now:
+        print(f'sleeping until lesson starts')
+        time.sleep((start - now).total_seconds())
+
+
+
+def loginToSystem(login, password):
+    print('\nAttempt to login ...')
+
+    textbox_mail = wait.until(
+        EC.presence_of_element_located((By.ID, 'Data_Mail'))
+    )
+    textbox_password = wait.until(
+        EC.presence_of_element_located((By.ID, 'Data_Password'))
+    )
+    login_button = wait.until(
+        EC.element_to_be_clickable((By.TAG_NAME, 'button'))
+    )
+    textbox_mail.send_keys(login)
+    textbox_password.send_keys(password)
+    login_button.click()
+    
+    print('Succeful login')
+
+
+
+def startLesson():
+    print('\nLoading page...')
+    while True:
+        try:
+            wait.until_not(
+                EC.visibility_of_element_located((By.ID, 'loader'))
+            )
+            lecture_button = driver.find_element(By.CLASS_NAME, 'timeline-content')
+            break
+        except:
+            pass
+
+    lecture_button.click()
+    print('Page loaded')
+    wait.until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/main/div/div[1]/div/div[7]/div/div/div/div/div/table/tbody/tr/td[5]/a'))
+    ).click()
+    print('Lesson starting...')
+
+
+
+def joinZoom():
+    print('\nWaiting for zoom...')
+    wait.until(EC.number_of_windows_to_be(2))
+
+    for window_handle in driver.window_handles:
+        if window_handle != original_window:
+            driver.switch_to.window(window_handle)
+            break
+
+    wait.until(EC.title_contains('Zoom'))
+
+    current_url = driver.current_url
+    new_url = current_url.replace('w/', 'wc/join/')
+    driver.get(new_url)
+    join_button = wait.until(EC.element_to_be_clickable((By.ID, 'joinBtn')))
+
+    join_button.click()
+    print('Zoom starting...')
+
+
+
+if __name__ == '__main__':
+    login, password = getLoginAndPassword()
+
+    lessonHours = getTodaysLessonHours()
+    lessonHours = getClosestHour(lessonHours)
+
+    print(f'Welcome! Today is {datetime.now().strftime("%A")}')
+    print(f'Today your have {len(lessonHours)} lecture')
+    print(f'In {" ".join([h[:5] for h in lessonHours])}')
+
+    driver  = webdriver.Firefox()
+    wait    = WebDriverWait(driver, 60)
+
+    driver.implicitly_wait(10)
+    driver.get('https://online.yildiz.edu.tr')
+    original_window = driver.current_window_handle
+
+    for hour in lessonHours:
+        startHour = datetime.strptime(
+            f'{datetime.now().strftime("%d/%m/%y")} {hour}', '%d/%m/%y %H:%M:%S')
+        
+        try:
+            loginToSystem(login, password)
+            startLesson()
+            joinZoom()
+        except Exception as e:
+            print(e)
+            driver.save_screenshot(f'{hour}-error.png')
+            driver.quit()
+            exit()
+    
+        waitUntil(startHour)
+    driver.quit()
     exit()
-    # debug numbers can be used to locate wherever the error occured
